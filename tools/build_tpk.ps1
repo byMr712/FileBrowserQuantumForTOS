f78af42095339cbf468c04ecea48e135c2a9b0a1
Mr712 <dzgrok@gmail.com>
2026-08-31 18:09:27 +0800

    minor update
    A minor update regarding the restructuring of the repository contents and the build process itself; this has no impact on the output .tpk files, meaning a rebuild of the GitHub release is not required.

--- tools/build_tpk.ps1
+++ tools/build_tpk.ps1
@@ -20,7 +20,7 @@
 
 .PARAMETER PkgDir
     Каталог с деревом пакета (относительно папки скрипта).
-    По умолчанию ..\FileBrowserQuantum_2.0.4.0_pkg
+    По умолчанию ..\FileBrowserQuantumTOS
 
 .PARAMETER OutDir
     Куда положить готовый .tpk (относительно папки скрипта). По умолчанию ..\ (filebrowser_new_build).
@@ -41,7 +41,7 @@
 #>
 [CmdletBinding()]
 param(
-    [string]$PkgDir = '..\FileBrowserQuantum_2.0.4.0_pkg',
+    [string]$PkgDir = '..\FileBrowserQuantumTOS',
     [string]$OutDir = '..',
     [string]$XzPath = '',
     [string]$ReleaseTag = 'beta'
